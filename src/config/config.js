8e7a0976bb61c995794a05cb1c327a3277f3ff5d
zhuangjiafeng <zhuangjiafeng@meizhitec.com>
2019-12-12 21:52:06 +0800

    页面标题配置

---
+++ src/config/config.js
@@ -0,0 +1,4 @@
+
+export default {
+  defaultTitle: 'Vue Template Webpack'
+}
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,35 @@
 import Vue from 'vue'
+import Config from '@/config/config'
 import Router from 'vue-router'
 import Home from '@/views/Home'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
       name: 'Home',
+      meta: {
+        title: '首页'
+      },
       component: Home
     },
     {
       path: '/card',
       name: 'Card',
-      component: () => import(/* webpackChunkName: "card" */ '../views/Card.vue')
+      meta: {
+        title: '卡片页'
+      },
+      component: () => import(/* webpackChunkName: "card" */ '../views/Card')
     }
   ]
 })
+
+// 页面标题
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title || Config.defaultTitle
+  next()
+})
+
+export default router
