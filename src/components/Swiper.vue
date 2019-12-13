81089e3512b0a6ee7acf86d1e89820ba0ded10f0
zhuangjiafeng <zhuangjiafeng@meizhitec.com>
2019-12-13 16:45:13 +0800

    swiper抽取

---
+++ src/components/Swiper.vue
@@ -0,0 +1,72 @@
+<template>
+  <swiper :options="swiperOption">
+    <swiper-slide v-for="(card, i) in list" v-bind:key="i">
+      <img :src="card.img" @click="onClickSlide(card)" />
+    </swiper-slide>
+    <div class="swiper-pagination" slot="pagination"></div>
+  </swiper>
+</template>
+
+<script>
+import 'swiper/dist/css/swiper.css'
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+
+export default {
+  name: 'swiper-component',
+  components: {
+    swiper,
+    swiperSlide
+  },
+  props: {
+    list: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data() {
+    return {
+      name: 'Swiper',
+      swiperOption: {
+        initialSlide: 0,
+        slidesPerView: 2,
+        spaceBetween: 5,
+        centeredSlides: true,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true
+        },
+        on: {
+          slideChange: this.onChangeSlide
+        }
+      }
+    }
+  },
+  methods: {
+    onChangeSlide() {
+      this.$emit('changeSlide')
+    },
+    onClickSlide(card) {
+      this.$emit('clickSlide', card)
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  @import '../style/common.less';
+  .swiper-container {
+    margin-top: ~`unit(24)`;
+    .swiper-slide {
+      // width: 60%;
+      height: ~`unit(480)`;
+      background: #eee;
+      overflow: hidden;
+      border-radius: ~`unit(16)`;
+      opacity: 1;
+      transition: transform .5s;
+      &:not(.swiper-slide-active) {
+        transform: scale(0.9);
+      }
+    }
+  }
+</style>
--- src/views/Card/Swiper.vue
+++ src/views/Card/Swiper.vue
@@ -1,56 +1,57 @@
 <template>
   <div class="page-wrapper">
     <div class="word">{{ name }}</div>
-    <swiper :options="swiperOption">
-      <swiper-slide v-for="(card, i) in cardList" v-bind:key="i">
-        <img :src="card.img" />
-      </swiper-slide>
-      <div class="swiper-pagination" slot="pagination"></div>
-    </swiper>
+    <swiper-view :list="cardList" />
+    <Button class="btn-lottery" type="primary" @click="startLottery">立即抽卡</Button>
+    <Popup class="popup-wrapper" v-model="showPopup">
+      <img :src="cardList[0].img || ''" />
+    </Popup>
   </div>
 </template>
 
 <script>
 import 'swiper/dist/css/swiper.css'
-import { swiper, swiperSlide } from 'vue-awesome-swiper'
+import swiper from '@/components/Swiper'
+import { Button, Popup } from 'vant'
 
 export default {
   name: 'Swiper',
   components: {
-    swiper,
-    swiperSlide
+    'swiper-view': swiper,
+    Button,
+    Popup
   },
   data() {
     return {
       name: 'Swiper',
+      showPopup: false,
       cardList: [
         {
           id: 1,
-          img: 'http://img5.imgtn.bdimg.com/it/u=3008142408,2229729459&fm=26&gp=0.jpg'
+          img: 'http://img5.imgtn.bdimg.com/it/u=3008142408,2229729459&fm=26&gp=0.jpg',
+          text: '爱岗福'
         },
         {
           id: 2,
-          img: 'http://img5.imgtn.bdimg.com/it/u=3008142408,2229729459&fm=26&gp=0.jpg'
+          img: 'http://img5.imgtn.bdimg.com/it/u=3008142408,2229729459&fm=26&gp=0.jpg',
+          text: '敬业福'
         },
         {
           id: 3,
-          img: 'http://img5.imgtn.bdimg.com/it/u=3008142408,2229729459&fm=26&gp=0.jpg'
+          img: 'http://img5.imgtn.bdimg.com/it/u=3008142408,2229729459&fm=26&gp=0.jpg',
+          text: '诚信福'
         },
         {
           id: 4,
-          img: 'http://img5.imgtn.bdimg.com/it/u=3008142408,2229729459&fm=26&gp=0.jpg'
+          img: 'http://img5.imgtn.bdimg.com/it/u=3008142408,2229729459&fm=26&gp=0.jpg',
+          text: '友善福'
         }
-      ],
-      swiperOption: {
-        initialSlide: 1,
-        slidesPerView: 2,
-        spaceBetween: 5,
-        centeredSlides: true,
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true
-        }
-      }
+      ]
+    }
+  },
+  methods: {
+    startLottery() {
+      this.showPopup = true
     }
   }
 }
@@ -65,20 +66,8 @@ export default {
       font-size: ~`unit(48)`;
       background: green;
     }
-  }
-  .swiper-container {
-    margin-top: ~`unit(24)`;
-    .swiper-slide {
-      // width: 60%;
-      height: ~`unit(480)`;
-      background: #eee;
-      overflow: hidden;
-      border-radius: ~`unit(16)`;
-      opacity: 1;
-      transition: transform .5s;
-      &:not(.swiper-slide-active) {
-        transform: scale(0.9);
-      }
+    .btn-lottery {
+      margin-top: ~`unit(32)`;
     }
   }
 </style>
