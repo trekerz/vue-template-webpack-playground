491c515d3723d270d5c70db7c1562e8b2a1b52d3
zhuangjiafeng <zhuangjiafeng@meizhitec.com>
2019-12-13 23:19:32 +0800

    去掉production sourcemap、修复webpack绝对路径问题

--- config/index.js
+++ config/index.js
@@ -50,13 +50,13 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '', // 没有斜杠为相对路径，加斜杠为绝对路径
 
     /**
      * Source Maps
      */
 
-    productionSourceMap: true,
+    productionSourceMap: false,
     // https://webpack.js.org/configuration/devtool/#production
     devtool: '#source-map',
 
